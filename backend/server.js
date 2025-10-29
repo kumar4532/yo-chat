a4f969a1da6ba3bdfda8901bf6aa1fdd36d4d009
kumar4532 <krantikumar2001@gmail.com>
2025-10-29 11:43:31 +0530

    refactor CORS middleware configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,14 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: [
+        "https://yo-chat-seven.vercel.app",
+        "http://localhost:3000"
+    ],
+    credentials: true
+}));
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cookieParser());
