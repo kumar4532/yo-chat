af46d0d955f6f84a86ad987dddcb813d186902b9
kumar4532 <krantikumar2001@gmail.com>
2026-02-02 20:37:07 +0530

    feat: add AuthSkeleton component for loading state in authentication pages; refactor loading handling in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import Home from "./pages/home/Home"
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp"
 import Profile from "./pages/profile/Profile"
 import Video from "./pages/call/Video"
 import Voice from "./pages/call/Voice"
+import AuthSkeleton from "./components/skeleton/AuthSkeleton"
 import { Toaster } from "react-hot-toast"
 import { useAuthContext } from "./context/AuthContext"
 import LayoutSkeleton from "./components/skeleton/LayoutSkeleton"
@@ -15,9 +16,17 @@ function App() {
   const { authUser, loading } = useAuthContext();
   useListenMessages();
 
+  const location = useLocation()
+
+  const isAuthPage =
+    location.pathname === "/login" ||
+    location.pathname === "/signup"
+
   if (loading) {
-    return <LayoutSkeleton />;
+    if (isAuthPage) return <AuthSkeleton />
+    if (authUser) return <LayoutSkeleton />
   }
+
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
     password: "",
   })
 
-  const {loading, login} = useLogin();
+  const { loading, login } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,44 +18,44 @@ function Login() {
 
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-            <h1 className='text-3xl font-semibold text-center text-gray-300'>Login
-              <span className='text-blue-300'> Yooo!!!</span>
-            </h1>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>Login
+          <span className='text-blue-300'> Yooo!!!</span>
+        </h1>
 
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label className='label p-2' htmlFor="">
-                  <span className='text-base label-text text-white'>Username</span>
-                </label>
-                <input 
-                type="text" 
-                placeholder='Enter Username' 
-                className='w-full input input-bordered h-10' 
-                value={inputs.username}
-                onChange={(e) => setInputs({...inputs, username:e.target.value})}/>
-              </div>
-              <div>
-                <label className='label p-2' htmlFor="">
-                  <span className='text-base label-text text-white'>Password</span>
-                </label>
-                <input 
-                type="password" 
-                placeholder='Enter Password' 
-                className='w-full input input-bordered h-10' 
-                value={inputs.password}
-                onChange={(e) => setInputs({...inputs, password:e.target.value})}/>
-              </div>
-              <Link to="/signup" className='text-sm hover:text-white mt-2 inline-block'>Don't have an account?</Link>
-              <div>
-                <button disabled={loading} className='btn btn-block btn-sm mt-2'>
-                  {
-                    loading ? <span className='loading loading-spinner'></span> : " Login"
-                  }
-                </button>
-              </div>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className='label p-2' htmlFor="">
+              <span className='text-base label-text text-white'>Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder='Enter Username'
+              className='w-full input input-bordered h-10'
+              value={inputs.username}
+              onChange={(e) => setInputs({ ...inputs, username: e.target.value })} />
+          </div>
+          <div>
+            <label className='label p-2' htmlFor="">
+              <span className='text-base label-text text-white'>Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder='Enter Password'
+              className='w-full input input-bordered h-10'
+              value={inputs.password}
+              onChange={(e) => setInputs({ ...inputs, password: e.target.value })} />
+          </div>
+          <Link to="/signup" className='text-sm hover:text-white mt-2 inline-block'>Don't have an account?</Link>
+          <div>
+            <button disabled={loading} className='btn btn-block btn-sm mt-2'>
+              {
+                loading ? <span className='loading loading-spinner'></span> : " Login"
+              }
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
